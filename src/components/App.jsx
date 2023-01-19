c928b242a6e03200a41ed004ea7d2f9c3ca9f6ae
Andrey Belokon <belokon1981andrey@gmail.com>
2023-01-19 18:46:33 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import css from '../components/App.module.css';
+import { useLocalStorage } from '../components/hooks/useLocalStorege';
+
 export const App = () => {
+  const [contacts, setContacts] = useLocalStorage('contacts', []);
+  const [filter, setFilter] = useState('');
+
+  const handleSubmit = ({ name, number }) => {
+    const id = nanoid();
+    const contactsLists = [...contacts];
+
+    if (contactsLists.findIndex(contact => name === contact.name) !== -1) {
+      alert(`${name} is already in contacts.`);
+    } else {
+      contactsLists.push({ name, id, number });
+    }
+
+    setContacts(contactsLists);
+  };
+
+  const getFilteredContacts = () => {
+    const filterContactsList = contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(filter.toLowerCase());
+    });
+
+    return filterContactsList;
+  };
+
+  const handleChange = e => {
+    const filter = e.target.value;
+    setFilter(filter);
+  };
+
+  const handleDelete = elementDeleteId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== elementDeleteId)
+    );
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework templatefhfhfh
+    <div className={css.app_style}>
+      <h1>Phonebook</h1>
+      <ContactForm handleSubmit={handleSubmit} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} handleChange={handleChange} />
+      <ContactList
+        contacts={getFilteredContacts()}
+        handleDelete={handleDelete}
+      />
     </div>
   );
 };
